613d73511aae8f7a72bd4ecbab67d038d88708dc
fanramdhan17 <100453567+fanramdhan17@users.noreply.github.com>
2022-03-04 17:40:13 +0700

    First Commit, Menambah GUI

---
+++ KPL_TPMOD2/KPL_TPMOD2/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KPL_TPMOD2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String nme = textBox1.Text;
+            MessageBox.Show("Hello "+nme);
+        }
+    }
+}
